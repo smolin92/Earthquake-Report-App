0b0aa5fb5ff3c4227e539ffe8a0d95e93276d511
Stefanie Molin <stefaniemolin@gmail.com>
2018-02-18 11:08:53 -0500

    Use regular expressions when parsing the location from the JSON data

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeArrayAdapter.java
@@ -0,0 +1,130 @@
+
+/**
+ * MIT License
+
+ Copyright (c) 2018 Stefanie Molin
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+package com.example.android.quakereport;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EarthquakeArrayAdapter extends ArrayAdapter {
+
+    public static final String LOG_TAG = EarthquakeArrayAdapter.class.getName();
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context        The current context. Used to inflate the layout file.
+     * @param earthquakes    A List of Earthquake objects to display in a list
+     */
+    public EarthquakeArrayAdapter(Activity context, ArrayList<Earthquake> earthquakes) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, earthquakes);
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Earthquake} object located at this position in the list
+        Earthquake currentEarthquake = (Earthquake) getItem(position);
+
+        // Provide the earthquake magnitude
+        TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude);
+        magnitudeTextView.setText(Double.toString(currentEarthquake.getMagnitude()));
+
+        // Pull in the location string, if this string doesn't have distance info, it will be used as is
+        // and the offset will be set to "Near the". If it does have distance info, that will be split out
+        String location = currentEarthquake.getLocation();
+        Pattern locationOffsetRegex = Pattern.compile("\\d*km [A-Z]* of ");
+        Matcher patternMatcher = locationOffsetRegex.matcher(location);
+        String locationOffset = "Near the";
+        if(patternMatcher.find()){
+            locationOffset = location.substring(patternMatcher.start(), patternMatcher.end());
+            location = location.substring(patternMatcher.end());
+        }
+
+        // Provide the earthquake location and offset
+        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
+        TextView locationOffsetTextView = (TextView) listItemView.findViewById(R.id.location_offset);
+        locationTextView.setText(location);
+        locationOffsetTextView.setText(locationOffset);
+
+        // Provide the earthquake date and time
+        Date datetime = new Date(currentEarthquake.getTime());
+        TextView dateTextView = (TextView) listItemView.findViewById(R.id.date);
+        TextView timeTextView = (TextView) listItemView.findViewById(R.id.time);
+        dateTextView.setText(formatDate(datetime));
+        timeTextView.setText(formatTime(datetime));
+
+
+        return listItemView;
+    }
+
+    /**
+     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
+     */
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObject);
+    }
+
+
+}
